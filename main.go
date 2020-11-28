e362e90d78428eb85ba26a96fa04cfaac2f44b0e
Thomas LACAZE <contact@thomaslacaze.fr>
2020-11-28 12:47:02 +0100

    refactor

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func main() {
+	r := setupRouter()
+	r.Run(":8083")
+}
+
 func setupRouter() (r *gin.Engine) {
 	r = gin.Default()
 
 	r.GET("/persons", GeneratePersonsHandler)
 	r.GET("/sentence", GenerateSentenceHandler)
 	return
-}
-
-func main() {
-	r := setupRouter()
-	r.Run(":8083")
 }
